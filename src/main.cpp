1822c4caad4ce45fbb0891b57d4b303bae7a8c83
tangbin <tangbin@hongkingsystem.cn>
2015-11-15 17:54:21 +0800

    second modified

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,15 @@
 #include "manager.h"
+#include "connection.h"
+
 #include <QtWidgets/QApplication>
 
 int main(int argc, char *argv[])
 {
+	if (!createConnection())
+	{
+		return 0;
+	}
+
 	QApplication a(argc, argv);
 	manager w;
 	w.show();
